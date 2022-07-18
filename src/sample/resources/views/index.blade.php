208ee736d137a258100bd831eb9bbfc1beef031e
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2022-07-19 04:41:07 +0900

    〜導線の確保(index→new)

--- src/sample/resources/views/index.blade.php
+++ src/sample/resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-サンプルです。
+<a href="{{route('memo.new')}}">新規メモ追加</a>
 <ul>
     @foreach($message as $memo)
     <li><a href="{{ route('memo.show', ['id' => $memo->id]) }}">
@@ -10,4 +10,9 @@
     </li>
     @endforeach
 </ul>
+{{-- <hr> ※ ↓はnew.blade.phpからお引っ越し index上で完結させたい場合の例(授業で実施)
+{{ Form::open(['route' => 'memo.store', 'method' => 'POST']) }}
+    {{ Form::text('text') }}
+    {{ Form::submit('新規作成') }}
+{{ Form::close() }} --}}
 @endsection
