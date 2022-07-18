dd3f1d15a348f556eaf0a9cb187f841a364b261a
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2022-07-19 04:23:13 +0900

    〜 layout

--- src/sample/resources/views/index.blade.php
+++ src/sample/resources/views/index.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サンプル</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
 サンプルです。
 <ul>
     @foreach($message as $memo)
@@ -16,6 +10,4 @@
     </li>
     @endforeach
 </ul>
-
-</body>
-</html>
+@endsection
